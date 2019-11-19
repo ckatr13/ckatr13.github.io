faebe588a6c658f3f47486bf5140c70ee9c0ee9a
ckatr13 <53585660+ckatr13@users.noreply.github.com>
2019-11-19 15:31:34 -0600

    Update effects.js

--- effects.js
+++ effects.js
@@ -198,3 +198,24 @@ function showTitle() {
     setTimeout(showWwd, 2000);
 }
 
+
+/* ============== SUPPORTS TOUCH OR NOT ========= */
+/*! Detects touch support and adds appropriate classes to html and returns a JS object
+  Copyright (c) 2013 Izilla Partners Pty Ltd  | http://www.izilla.com.au 
+  Licensed under the MIT license  |  https://coderwall.com/p/egbgdw 
+*/
+var supports = (function() {
+    var d = document.documentElement,
+        c = "ontouchstart" in window || navigator.msMaxTouchPoints;
+    if (c) {
+        d.className += " touch";
+        return {
+            touch: true
+        }
+    } else {
+        d.className += " no-touch";
+        return {
+            touch: false
+        }
+    }
+})();
